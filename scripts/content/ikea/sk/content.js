8cabf4c0c97689c53aa2997e8dbe2504f337b821
Christoph <Macusercom@users.noreply.github.com>
2023-05-31 14:07:41 +0200

    Added IKEA Slovakia and inverse country check
    IKEA Slovakia will be checked too and the prices of the other stores also will now be displayed on non-Austrian websites

---
+++ scripts/content/ikea/sk/content.js
@@ -0,0 +1,53 @@
+function getPriceElement() {
+  return document.getElementsByClassName("pip-temp-price")[0];
+}
+
+function extractPriceFromText(text) {
+  const regex = /"price":\w*\["([^"]*)"],/;
+  console.log(regex.exec(text))
+  return regex.exec(text)[1];
+}
+
+async function fetchPrice(url) {
+  try {
+    const response = await new Promise(resolve => {
+      chrome.runtime.sendMessage({ url: url }, resolve);
+    })
+
+    return (response && response.found) ? extractPriceFromText(response.text) : null;
+  } catch {
+    return null;
+  }
+}
+
+async function getLocalPrice() {
+  const script = document.createElement("script");
+  script.src = chrome.runtime.getURL("scripts/helper/extract_utag_data.js");
+  document.head.appendChild(script);
+
+  return new Promise(resolve => {
+    window.addEventListener("message", ({data}) => {
+      if (data.id != "price-checker-data") return;
+      resolve(data.price);
+    }, false);
+  });
+}
+
+async function refreshPrice() {
+  const localPrice = await getLocalPrice();
+  const otherPriceAT = await fetchPrice(document.location.href.replace("/sk/sk", "/at/de"));
+  const otherPriceDE = await fetchPrice(document.location.href.replace("/sk/sk", "/de/de"));
+
+  if (localPrice) {
+    const priceElement = getPriceElement();
+    priceElement.innerHTML = "SK: € " + localPrice + " <br /> AT: € " + (otherPriceAT ?? "N/A") + " <br /> SK: € " + (otherPriceDE ?? "N/A");
+  }
+}
+
+chrome.runtime.onMessage.addListener(async function (request, _, _) {
+  if (request.message === 'refreshPrice') {
+    window.setTimeout(async () => {
+      await refreshPrice();
+    }, 1000);
+  }
+});
